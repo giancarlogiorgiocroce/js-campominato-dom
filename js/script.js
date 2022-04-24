8ccc07291c483a275c77f6024645c7d054eb73ec
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-04-24 12:55:58 +0200

    Aggiunto contatore punti e condizione di perdita con bombe, amministrate da this

--- js/script.js
+++ js/script.js
@@ -24,9 +24,14 @@ Le validazioni e i controlli possiamo farli anche in un secondo momento.
 
 // Variabili universali
 const container = document.querySelector(".container");
-const BOMBE_MASSIME = 16;
-let livelloSfida;
 
+const BOMBE_MASSIME = 16;
+let punteggio = document.querySelector("main div h1");
+let block = document.querySelector(".block")
+let contatorePunteggio = 0;
+let bombe;
+// let arrayVittoria = [];
+let livelloSfida;
 
 // Logica
 document.querySelector('#livello').addEventListener("change", assegnaLv);
@@ -35,64 +40,113 @@ document.querySelector("#btn").addEventListener("click", inizia);
 
 // Funzioni
 function inizia(){
-
+    let box;
+    
     pulisci();
     assegnaLv();
-    // creaFinestraRisultati();
+    mostraFinestraRisultati();
 
+    
+    bombe = generaListaBombe(1, livelloSfida);
+    // console.log(bombe);
+    
     for(let i = 1; i <= livelloSfida; i++){
-        const box = creaCelle(container);
+        box = creaCelle(container);
         box.append(i);
+        // arrayVittoria.push(i);
+        
+
+            if(bombe.includes(i)){
+                box.classList.add("bomb");
+            }
 
         box.addEventListener('click', amministraCelleCliccate);
+        
+        // fineGioco(i);
     }
-
+    
 }
-    
-    function pulisci(){
-        container.innerHTML = "";
-    }
-    
-    function assegnaLv(){
-        livelloSfida = livello.options[livello.selectedIndex].value;
-    }
 
-    // function creaFinestraRisultati(){
-    //     const risultati = document.querySelector("resoults");
-    //     const window = document.createElement("div");
 
-    //     window.classList.add("window");
-    //     risultati.append(window);
 
-    // }
+function pulisci(){
+    container.innerHTML = "";
+    contatorePunteggio = 0;
+    punteggio.innerHTML = contatorePunteggio;
+    document.getElementById("endgame").innerHTML = "";
+    block.classList.add("invisible");
+}
 
-    function creaCelle (doveCreare){
-        const box = document.createElement("div");
-        box.classList.add("box", "center", "box"+livelloSfida);
-        doveCreare.append(box);
-        return box;
-    }
+function assegnaLv(){
+    livelloSfida = livello.options[livello.selectedIndex].value;
+}
 
-    function amministraCelleCliccate(){
+function mostraFinestraRisultati(){
+    const risultati = document.querySelector(".results");
+    risultati.classList.remove("invisible");
+}
+
+
+/*
+    GENERA TUTTO QUELLO CHE è NEL CONTAINER
+*/
+function creaCelle (doveCreare){
+    const box = document.createElement("div");
+    box.classList.add("box", "center", "box"+livelloSfida);
+
+    doveCreare.append(box);
+    return box;
+}
+
+function amministraCelleCliccate(){
+    // let punteggioMassimo = livelloSfida - BOMBE_MASSIME;
+
+    if(this.classList.contains("bomb")){
         this.classList.add('active');
+        // bombe.className += " active";
+        // console.log(bombe);
+        document.getElementById("endgame").innerHTML = `Spiacente, hai perso (ma con ${contatorePunteggio} punti totali!)`
+        punteggio.innerHTML = contatorePunteggio;
+        block.classList.remove("invisible");
+
+    }else if(this.classList.contains("active")){
+        alert("Non puoi premere due volte lo stesso campo!")
+
+    } else if(!this.classList.contains("active")){
+        this.classList.add('active');
+        contatorePunteggio++;
+        punteggio.innerHTML = contatorePunteggio;
     }
 
-        // console.log(generaListaBombe(1, BOMBE_MASSIME));
+};
 
-        function generaListaBombe(min, max){
-            let bomba;
-            let listaBombe = [];
-        
-            while(listaBombe.length<BOMBE_MASSIME){
-                bomba = generaNumeroCasuale(min, max);
-                if(!listaBombe.includes(bomba)){
-                    listaBombe.push(bomba);
-                }
-            }
+    // GENERATORE RANDOMICO DI BOMBE
+function generaListaBombe(min, max){
+    let listaBombe = [];
 
-            return bomba;
+    while(listaBombe.length<BOMBE_MASSIME){
+        bombe = generaNumeroCasuale(min, max);
+        if(!listaBombe.includes(bombe)){
+            listaBombe.push(bombe);
         }
-        
-            function generaNumeroCasuale(min, max) {
-                return Math.floor(Math.random() * (max - min + 1) + min);
-            }
+    }
+    console.log(listaBombe);
+    return listaBombe;
+}
+
+function generaNumeroCasuale(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+/*
+    /GENERA TUTTO QUELLO CHE è NEL CONTAINER
+*/
+
+// function fineGioco(quadratini){
+//     let arrayVittoria = [];
+//     arrayVittoria.push(quadratini);
+//     console.log(arrayVittoria);
+
+//     // if(array.classList.contains("active")){
+//     //     console.log("sei bello");
+//     // }
+// }
